3783f692c5f4401a92d805a370c6bb6f153df2e9
Yusuf Celep <yusufcelep8@gmail.com>
2020-02-20 02:00:37 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,43 @@
-# code here!
+class School
+  # attr_accessor :new, :roster, :add_student, :grade
+  
+  # def initialize(new)
+  #   @new = new
+  # end
+  
+  # def roster
+  #   hash = {}
+  # end
+  
+  # def add_student(name, grade)
+  #   self.roster = hash[grade] = []
+  #   self.roster = hash[grade] << name
+  # end
+    def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def roster
+    @roster
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] != nil
+      @roster[grade] << name
+    else
+      @roster[grade] = [name]
+    end
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each do |key, value|
+      value.sort!
+    end
+  end
+end
+
